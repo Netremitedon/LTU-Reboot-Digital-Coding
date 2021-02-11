13aa98f200db2a4a83bd8c88abd1a37acbf70f6e
Cedric Netremitedon <Netremitedon@users.noreply.github.com>
2021-02-11 20:10:51 +0000

    Update task-3.1.2.js
    Added different type of comments
    Added alert/ consol log/ document write

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,11 @@
+/*
+ This is a 
+ multiple line
+ comment
+*/
+
+// this is a single line comment
+
+alert('Hello From the alert box'); // this pops up with an alert message
+console.log('Hello from the console'); // shows a message in the console
+document.write('Hello from the document.write'); // prints a message to the screen
