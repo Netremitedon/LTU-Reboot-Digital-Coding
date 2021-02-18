f80133737f2f99445489c59a9d1d634327431cc0
Cedric Netremitedon <Netremitedon@users.noreply.github.com>
2021-02-18 15:01:57 +0000

    Task 3.2.2
    COmpleted task 3.2.2

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,13 @@
+function getFullName(firstName, lastName){
+    let fullName = firstName + ' ' + lastName;
+    console.log(fullName)
+}
+
+var fName = 'Cedric';
+var lName = 'Demolis'
+
+var fName2 = 'Cedric';
+var lName2 = 'Netremitedon';
+
+getFullName(fName, lName);
+getFullName( fName2, lName2);
