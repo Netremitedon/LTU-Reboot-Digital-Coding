b51d34682a5422f5640a2bfb0a2e8807adf8cd95
Cedric Netremitedon <Netremitedon@users.noreply.github.com>
2021-02-23 19:55:57 +0000

    Task 3.4.1 done

---
+++ module-3/lesson-4/scripts/task-3.4.1.js
@@ -0,0 +1,11 @@
+for (multiplyer =1; multiplyer<=12; multiplyer++){
+  for(
+     counter=1; //set the value of the counter
+     counter<=12; // conditional logic
+     counter++ // add 1 to counter after each iteration
+     ){
+     let sum = x * counter;
+     let message = x + '*' + counter + '=' + sum;
+     console.log(message);
+    }
+}    
