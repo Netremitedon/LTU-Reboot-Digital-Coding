7b2481ab4c512a8fa57316fb793eb86fa43e5b1f
Cedric Netremitedon <Netremitedon@users.noreply.github.com>
2021-03-02 19:52:35 +0000

    completed task 3.6.2

---
+++ module-3/lesson-6/scripts/task-3.6.2.js
@@ -0,0 +1,7 @@
+var newElement = document.createElement('p');
+// console.log(newElement);
+var divElement = document.querySelector('div');
+// console.log(divElement);
+newElement.innerHTLM = 'Cedric is great';
+console.log(newElement);
+divElement.append(newElement);
