31cd0759ad9bb51403f8fef110d7b8c2e25a3607
Cedric Netremitedon <Netremitedon@users.noreply.github.com>
2021-03-02 20:54:35 +0000

    complete task 3.6.5

---
+++ module-3/lesson-6/scripts/task-3.6.5.js
@@ -0,0 +1,23 @@
+var submitButton = document.querySelector('.submit');
+
+function squareNumber(event){
+    event.preventDefault();
+    document.querySelector('#full-name').nodeValue;
+    var message = '';
+    if(!isNaN(value)){
+       message = 'you must enter a number to do maths'
+    } else {
+        message = (value * value);
+    }
+    document.querySelector('.message').innerHTML = message;
+
+}
+function greeting(event){
+    event.preventDefault();
+    let value = document.querySelector('#full-name').value;
+    var message = 'Hello ' + value;
+    document.querySelector('.message').innerHTML = message;
+}
+
+submitButton.addEventListener('click',squareNumber);
+    
